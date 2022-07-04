edd132dc4dc9c80f9afa85c9b8b2a75510d37c61
ji junhyuk <junto@jis-MacBook-Pro.local>
2022-07-04 13:42:33 +0900

    r

--- delete_issue.py
+++ delete_issue.py
@@ -1,29 +1,45 @@
 import pyautogui
 import time
-from pynput.mouse import Listener, Controller, Button
 
-mouse = Controller()
+# issue page
+pyautogui.moveTo(1139, 409)
+time.sleep(1)
+pyautogui.click()
 
-pyautogui.moveTo(100, 200)
-# mouse.click(Button.left, 2)
-# time.sleep(1)
- #while (1):
- #    # closed click
- #    pyautogui.moveTo(212, 408)
- #    pyautogui.click()
- #    time.sleep(1)
- #
- #    # top issue click
- #    pyautogui.moveTo(110, 496)
- #    pyautogui.click()
- #    time.sleep(1)
- #
- #    # delete issue click
- #    pyautogui.moveTo(999, 1231)
- #    pyautogui.click()
- #    time.sleep(1)
- #
- #    # click check
- #    pyautogui.moveTo(630, 519)
- #    pyautogui.click()
- #    time.sleep(1)
+pyautogui.moveTo(1017, 404)
+time.sleep(1)
+pyautogui.click()
+time.sleep(3)
+
+pyautogui.moveTo(981, 546)
+pyautogui.click()
+time.sleep(3)
+
+for i in range(15):
+    pyautogui.scroll(-10)
+time.sleep(2)
+pyautogui.click(1482, 826)
+time.sleep(2)
+pyautogui.click(1310, 474)
+while (1):
+    # closed click
+    time.sleep(3)
+    pyautogui.moveTo(1031, 469)
+    pyautogui.click()
+
+    # top issue click
+    pyautogui.moveTo(972, 545)
+    time.sleep(3)
+    pyautogui.click()
+
+    # delete issue click
+    time.sleep(3)
+    for i in range(15):
+        pyautogui.scroll(-10)
+    time.sleep(2)
+    pyautogui.moveTo(1469, 604)
+    pyautogui.click()
+
+    # warning check
+    time.sleep(2)
+    pyautogui.click(1310, 474)
--- s.py
+++
@@ -1,3 +0,0 @@
-import pyautogui
-
-pyautogui.moveTo(200, 0)
